89c877cb418aec1a22d27a91fa938e77bbe9e31f
Somya6464 <somyanaiwal023@gmail.com>
2026-06-26 01:45:49 +0530

    create a mono repo setup.

--- config.py
+++ config.py
@@ -10,3 +10,13 @@ class Settings:
     origins = os.getenv("origins")
 
 settings = Settings()
+
+
+"""
+origins = [
+    "http://localhost:3000",
+]
+ALGORITHM = "HS256"
+SECRET_KEY = "noSecretKey"
+db_url= "postgresql://postgres:123456@localhost:5000/bookstore"
+"""
